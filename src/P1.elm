5248909f7929cfc78f76f543bc05ffbebba077ea
Diogo Korok <korokd@protonmail.ch>
2020-12-06 16:00:01 -0300

    Create failing test

---
+++ src/P1.elm
@@ -0,0 +1,6 @@
+module P1 exposing (findSumOfMultiplesOfOrBetween)
+
+
+findSumOfMultiplesOfOrBetween : List Int -> ( Int, Int ) -> Int
+findSumOfMultiplesOfOrBetween possibleMultipliers ( exclusiveHead, inclusiveTail ) =
+    0
---
+++ tests/TestP1.elm
@@ -0,0 +1,15 @@
+module TestP1 exposing (findSumOfMultiplesOfOrBetweenTest)
+
+import Expect
+import P1 exposing (findSumOfMultiplesOfOrBetween)
+import Test exposing (Test, describe, test)
+
+
+findSumOfMultiplesOfOrBetweenTest : Test
+findSumOfMultiplesOfOrBetweenTest =
+    describe "If we list all the natural numbers below 10 that are multiples of 3 or 5"
+        [ test "their sum should be 23" <|
+            \_ ->
+                findSumOfMultiplesOfOrBetween [ 3, 5 ] ( 0, 10 )
+                    |> Expect.equal 23
+        ]
