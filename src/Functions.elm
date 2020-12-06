cf9c068981b6f247b26b3ec2ce4a44e7bc631ce0
Diogo Korok <korokd@protonmail.ch>
2020-12-06 17:28:27 -0300

    Implement body that passes the test

--- src/Functions.elm
+++ src/Functions.elm
@@ -1,9 +1,32 @@
 module Functions exposing (findSumOfMultiplesOfOrBetween, makeFibonacciSequence)
 
 
+addOneTermToReversedFibonacciSequence : Int -> List Int -> List Int
+addOneTermToReversedFibonacciSequence upperBound reversedSequence =
+    let
+        newTerm =
+            reversedSequence
+                |> List.take 2
+                |> List.sum
+    in
+    if newTerm > upperBound then
+        reversedSequence
+
+    else
+        newTerm
+            :: reversedSequence
+            |> addOneTermToReversedFibonacciSequence upperBound
+
+
 makeFibonacciSequence : ( Int, Int ) -> Int -> List Int
 makeFibonacciSequence ( first, second ) upperBound =
-    []
+    let
+        reversedSequence =
+            [ second, first ]
+    in
+    reversedSequence
+        |> addOneTermToReversedFibonacciSequence upperBound
+        |> List.reverse
 
 
 findSumOfMultiplesOfOrBetween : List Int -> ( Int, Int ) -> Int
