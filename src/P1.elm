4bf22a70b06ab1a4f3622226ed1d9c2f59c5524a
Diogo Korok <korokd@protonmail.ch>
2020-12-06 16:21:11 -0300

    Implement function that passes the test

--- src/P1.elm
+++ src/P1.elm
@@ -3,4 +3,16 @@ module P1 exposing (findSumOfMultiplesOfOrBetween)
 
 findSumOfMultiplesOfOrBetween : List Int -> ( Int, Int ) -> Int
 findSumOfMultiplesOfOrBetween possibleMultipliers ( head, tail ) =
-    0
+    let
+        list =
+            List.range head tail
+
+        multiples =
+            list
+                |> List.filter
+                    (\value ->
+                        possibleMultipliers
+                            |> List.any (\multiplier -> modBy multiplier value == 0)
+                    )
+    in
+    List.sum multiples
