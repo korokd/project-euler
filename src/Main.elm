3185e642ed9589e8e53061f382c6986f8fc9d801
Diogo Korok <korokd@protonmail.ch>
2020-12-06 16:52:36 -0300

    Add link to verify solutions
    Also renamed `allSolutions` to `mySolutions`

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,31 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, li, text, ul)
+import Html exposing (Html, a, div, li, text, ul)
+import Html.Attributes exposing (href, rel, target)
 import P1
 
 
-allSolutions : List ( String, Int )
-allSolutions =
+mySolutions : List ( String, Int )
+mySolutions =
     [ ( "Problem 1", P1.solution )
     ]
 
 
-main : Html msg
-main =
+checkSolutionsView : Html msg
+checkSolutionsView =
+    a
+        [ href "https://github.com/luckytoilet/projecteuler-solutions/blob/master/Solutions.md"
+        , target "_blank"
+        , rel "noreferrer noopener"
+        ]
+        [ text "You can verify the answers here" ]
+
+
+mySolutionsView : Html msg
+mySolutionsView =
     ul
         []
-        (allSolutions
+        (mySolutions
             |> List.map
                 (\( problem, solution ) ->
                     li []
@@ -23,3 +34,12 @@ main =
                         ]
                 )
         )
+
+
+main : Html msg
+main =
+    div
+        []
+        [ checkSolutionsView
+        , mySolutionsView
+        ]
